10a3c1502df46921d4ee16ff943afe2024a119e8
root <root@vmi100241.contabo.host>
2017-03-02 21:35:28 +0100

    first commit

---
+++ email.php
@@ -0,0 +1,6 @@
+<?php
+if(mail("darioajr@gmail.com","A Subject Here","Hi there,nThis email was sent using PHP's mail function."))
+print "Email successfully sent";
+else
+print "An error occured";
+?>
